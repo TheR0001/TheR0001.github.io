436a736765ec23ceb2dbeb884248d9789a1bcf51
TheR0001 <164377596+TheR0001@users.noreply.github.com>
2024-08-02 13:24:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const heroContent = document.querySelector('.hero-content');
     heroContent.classList.add('fade-in');
 });
-
